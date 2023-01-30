4d0553932a3b122c0fc894dfd79599613499e278
Jimmy <jimmy@skelios.com>
2023-01-30 16:55:09 -0500

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <div className="App">
-      
+      Modify me
     </div>
   )
 }
